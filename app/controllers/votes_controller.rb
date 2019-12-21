706be6a25373bf56258bb886cbf82930b63843b7
roshita02 <roshitashakya07@gmail.com>
2019-12-21 16:28:04 +0545

    added updates

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,10 @@
 class VotesController < ApplicationController
   def index
     @votes = Vote.totals
+    respond_to do |format|
+      format.html
+      format.json { render json: @votes }
+    end
   end
 
   def create
